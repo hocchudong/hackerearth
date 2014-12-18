08a6016ce1e996bc87fe2e4ceb7496aeb684ed78
trananhkma <trananhkma@gmail.com>
2014-12-19 02:16:56 +0700

    Changes to be committed: 	modified:   Python/8.py

--- Python/8.py
+++ Python/8.py
@@ -4,19 +4,19 @@ a = input()
 c = []
 
 for i in range(a):
-	b = raw_input()
-	c.append(b)
+    b = raw_input()
+    c.append(b)
 
 mau = "qwertyuioplkjhgfdsazxcvbnm"
 for i in c:
-	if len(i) < 26:
-		print "NO"
-	else:
-		dem = 0
-		for j in mau:
-			if j in i:
-				dem += 1
-		if dem == len(mau):
-			print "YES"
-		else:
-			print "NO"
+    if len(i) < 26:
+        print "NO"
+    else:
+        dem = 0
+        for j in mau:
+            if j in i:
+                dem += 1
+        if dem == len(mau):
+            print "YES"
+        else:
+            print "NO"
